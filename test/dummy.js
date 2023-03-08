69994397c4ea845c6718834c1515c54a15f7ca12
Nayaka90 <113663282+Nayaka90@users.noreply.github.com>
2023-03-08 18:15:10 +0530

    hi

--- test/dummy.js
+++
@@ -1,6 +0,0 @@
-var str="i am working in testyantra"
-console.log(convertFirstCh(str))
-function convertFirstCh(str){
- str=str.split(' ').map((word)=>word[0].toUpperCase()+word.slice(1).toLowerCase()).join(' ')
-return str
-}
